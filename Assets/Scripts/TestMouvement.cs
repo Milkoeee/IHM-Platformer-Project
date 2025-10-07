cbc82023c8ad1f772b1293b317d89257a9269b12
raphh8 <149191554+raphh8@users.noreply.github.com>
2025-10-07 16:47:25 +0200

    add crouching

--- Assets/Scripts/TestMouvement.cs
+++ Assets/Scripts/TestMouvement.cs
@@ -8,11 +8,17 @@ public class TestMouvement : MonoBehaviour
     InputAction moveAction;
     InputAction AButton;
     InputAction sprintAction;
+    InputAction crouchAction;
+
     public Rigidbody2D rb;
+    public BoxCollider2D bc2d;
+
     public float jumpSpeed = 20;
     public float sprintFactor = 2.0f;
+    public float crouchFactor = 0.5f;
 
-    public float speedMofifier = 5;
+    public float speedModifier = 5;
+    public float currentSpeed;
 
     bool maxJump = false;
     bool isJumping = false;
@@ -20,26 +26,55 @@ public class TestMouvement : MonoBehaviour
     [SerializeField] float maxJumpTime = 1;
     float curJumpTime = 0;
 
+    public bool isCrouched;
+    public float crouchHold;
+    public Vector3 originalScale;
+
     private void Start()
     {
         // 3. Find the references to the "Move" and "Jump" actions
         moveAction = InputSystem.actions.FindAction("Move");
         AButton = InputSystem.actions.FindAction("Jump");
         sprintAction = InputSystem.actions.FindAction("Sprint");
+        crouchAction = InputSystem.actions.FindAction("Crouch");
 
         rb = GetComponent<Rigidbody2D>();
+
+        originalScale = transform.localScale;
     }
 
     void Update()
     {
         // 4. Read the "Move" action value, which is a 2D vector
         // and the "Jump" action state, which is a boolean value
-
-        bool isSprinting = sprintAction.IsPressed();
-        float currentSpeed = isSprinting ? speedMofifier * sprintFactor : speedMofifier;
-
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
-        // your movement code here
+
+        bool isSprinting = sprintAction.IsPressed() && !isCrouched;
+        currentSpeed = isSprinting ? speedModifier * sprintFactor : isCrouched ? speedModifier * crouchFactor : speedModifier;
+
+        if (crouchAction.IsPressed())
+        {
+            crouchHold += Time.deltaTime;
+            if (crouchHold >= 0.8f && !isCrouched)
+            {
+                isCrouched = true;
+                transform.localScale = new Vector3(originalScale.x, originalScale.y * crouchFactor, originalScale.z);
+                float difference = (transform.localScale.y - originalScale.y) / 2f;
+                transform.position += Vector3.up * difference;
+            }
+        }
+        else
+        {
+            crouchHold = 0f;
+        }
+
+        if (isCrouched && !crouchAction.IsPressed())
+        {
+            isCrouched = false;
+            float difference = (originalScale.y - transform.localScale.y) / 2f;
+            transform.localScale = originalScale; 
+            transform.position += Vector3.up * difference;
+        }
 
         rb.linearVelocityX = currentSpeed * moveValue.x;
 
