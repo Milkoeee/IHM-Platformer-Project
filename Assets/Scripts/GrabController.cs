58e8b6a32486140cd97c1accabf6fd6414198f0f
raphh8 <149191554+raphh8@users.noreply.github.com>
2025-10-07 17:42:33 +0200

    add grab

---
+++ Assets/Scripts/GrabController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GrabController : MonoBehaviour
+{
+    public Transform boxHolder;
+    public Transform grabDetection;
+    public float rayDist;
+
+    InputAction grabAction;
+
+    void Start()
+    {
+        grabAction = InputSystem.actions.FindAction("Grab");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D grabCheck = Physics2D.Raycast(grabDetection.position, Vector2.right * transform.localScale, rayDist);
+
+        if (grabCheck.collider != null && grabCheck.collider.tag == "Box")
+        {
+            if(grabAction.IsPressed())
+            {
+                Debug.Log("Grab");
+                grabCheck.collider.gameObject.transform.parent = boxHolder;
+                grabCheck.collider.gameObject.transform.position = boxHolder.position;
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+            }
+            else
+            {
+                grabCheck.collider.gameObject.transform.parent = null;
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            }
+        }
+    }
+}
