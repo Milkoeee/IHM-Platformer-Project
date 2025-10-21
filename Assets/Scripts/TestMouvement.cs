62bdfc4a32b1caa013362ec418b5235b5efdcebe
raphh8 <149191554+raphh8@users.noreply.github.com>
2025-10-21 17:05:41 +0200

    Merging movement script

--- Assets/Scripts/TestMouvement.cs
+++ Assets/Scripts/TestMouvement.cs
@@ -13,12 +13,18 @@ public class TestMouvement : MonoBehaviour
     InputAction moveAction;
     InputAction AButton;
     InputAction sprintAction;
+    InputAction crouchAction;
+
     public Rigidbody2D rb;
+    public BoxCollider2D bc2d;
+
     public float jumpSpeed = 20;
     public float sprintFactor = 2.0f;
+    public float crouchFactor = 0.5f;
 
-    public float speedMofifier = 5;
+    public float speedModifier = 5;
     public float maxJumpTime = 1;
+    public float currentSpeed;
 
     bool maxJump = false;
     bool isJumping = false;
@@ -43,24 +49,63 @@ public class TestMouvement : MonoBehaviour
     Queue<float> bufferQueue = new Queue<float>();
     BoxCollider2D collider;
 
+    public bool isCrouched;
+    public float crouchHold;
+    public Vector3 originalScale;
+
     private void Start()
     {
         layerMask = LayerMask.GetMask("Wall");
         moveAction = InputSystem.actions.FindAction("Move");
         AButton = InputSystem.actions.FindAction("Jump");
         sprintAction = InputSystem.actions.FindAction("Sprint");
+        crouchAction = InputSystem.actions.FindAction("Crouch");
 
         rb = GetComponent<Rigidbody2D>();
         collider = GetComponent<BoxCollider2D>();
         rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+
+        originalScale = transform.localScale;
     }
 
     void Update()
     {
-        bool isSprinting = sprintAction.IsPressed();
-        float currentSpeed = isSprinting ? speedMofifier * sprintFactor : speedMofifier;
-
+        // 4. Read the "Move" action value, which is a 2D vector
+        // and the "Jump" action state, which is a boolean value
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
+
+        if (moveValue.x < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180f, 0);
+        }
+        else if (moveValue.x > 0) transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        bool isSprinting = sprintAction.IsPressed() && !isCrouched;
+        currentSpeed = isSprinting ? speedModifier * sprintFactor : isCrouched ? speedModifier * crouchFactor : speedModifier;
+
+        if (crouchAction.IsPressed())
+        {
+            crouchHold += Time.deltaTime;
+            if (crouchHold >= 0.8f && !isCrouched)
+            {
+                isCrouched = true;
+                transform.localScale = new Vector3(originalScale.x, originalScale.y * crouchFactor, originalScale.z);
+                float difference = (transform.localScale.y - originalScale.y) / 2f;
+                transform.position += Vector3.up * difference;
+            }
+        }
+        else
+        {
+            crouchHold = 0f;
+        }
+
+        if (isCrouched && !crouchAction.IsPressed())
+        {
+            isCrouched = false;
+            float difference = (originalScale.y - transform.localScale.y) / 2f;
+            transform.localScale = originalScale; 
+            transform.position += Vector3.up * difference;
+        }
 
         totalSpeed = currentSpeed * moveValue.x;
 
